544c98d82bd29d0e1bd94b9ed89797aa987378e1
kartikprabhu <prabhukartik20@gmail.com>
2026-02-11 19:27:31 +0530

    #modified packages to run with latest versions.

--- app/src/main/java/com/mintanable/notethepad/MainActivity.kt
+++ app/src/main/java/com/mintanable/notethepad/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -22,45 +24,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContent{
-            NoteThePadTheme{
-                Surface(
-                    color= MaterialTheme.colors.background
-                ){
-                    val navController= rememberNavController()
-                    NavHost(
-                        navController=navController,
-                        startDestination = Screen.NotesScreen.route
-                    ){
-                        composable(route= Screen.NotesScreen.route){
-                            NotesScreen(navController = navController)
-                        }
-                        composable(
-                            route= Screen.AddEditNoteScreen.route + "?noteId={noteId)&noteColor={noteColor}",
-                            arguments = listOf(
-                                navArgument(
-                                    name="noteId"
-                                ){
-                                    type = NavType.IntType
-                                    defaultValue = -1
-                                } ,
-                                navArgument(
-                                        name="noteColor"
-                                        ){
-                                    type = NavType.IntType
-                                    defaultValue = -1
-                                }
-                            )
-                        ){
-
-                            val color = it.arguments?.getInt("noteColor")?:-1
-                            AddEditNoteScreen(
-                                navController = navController,
-                                noteColor = color)
-                        }
-                    }
-                }
-
+        setContent {
+            NoteThePadTheme {
+                NoteScreen()
             }
         }
 //        binding = ActivityMainBinding.inflate(layoutInflater)
@@ -99,4 +65,54 @@ class MainActivity : AppCompatActivity() {
 //        return navController.navigateUp(appBarConfiguration)
 //                || super.onSupportNavigateUp()
 //    }
+}
+
+
+@Composable
+fun NoteScreen() {
+
+    Surface(
+        color= MaterialTheme.colors.background
+    ){
+        val navController= rememberNavController()
+        NavHost(
+            navController=navController,
+            startDestination = Screen.NotesScreen.route
+        ){
+            composable(route= Screen.NotesScreen.route){
+                NotesScreen(navController = navController)
+            }
+            composable(
+                route= Screen.AddEditNoteScreen.route + "?noteId={noteId)&noteColor={noteColor}",
+                arguments = listOf(
+                    navArgument(
+                        name="noteId"
+                    ){
+                        type = NavType.IntType
+                        defaultValue = -1
+                    } ,
+                    navArgument(
+                        name="noteColor"
+                    ){
+                        type = NavType.IntType
+                        defaultValue = -1
+                    }
+                )
+            ){
+
+                val color = it.arguments?.getInt("noteColor")?:-1
+                AddEditNoteScreen(
+                    navController = navController,
+                    noteColor = color)
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewConversation() {
+    NoteThePadTheme {
+        NoteScreen()
+    }
 }
