e76891f2091d582cba52f15918b98f6e54652ce7
kartikprabhu <prabhukartik20@gmail.com>
2026-02-27 19:52:13 +0530

    #merged audio and video player into one.

--- app/src/main/java/com/mintanable/notethepad/feature_note/presentation/modify/components/VideoPlayerUI.kt
+++ app/src/main/java/com/mintanable/notethepad/feature_note/presentation/modify/components/VideoPlayerUI.kt
@@ -1,16 +1,11 @@
 package com.mintanable.notethepad.feature_note.presentation.modify.components
 
-import android.net.Uri
 import androidx.annotation.OptIn
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.viewinterop.AndroidView
-import androidx.media3.common.MediaItem
-import androidx.media3.common.Player
 import androidx.media3.common.util.UnstableApi
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.ui.AspectRatioFrameLayout
